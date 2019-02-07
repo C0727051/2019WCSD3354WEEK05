0e0f0c548cbc050134fb1d080f6767a97446b38b
visualstudio <visualstudio@230-PC36>
2019-02-07 16:55:43 -0500

    commit at 4:55

--- 2019WCSD3354WEEK05/Program.cs
+++ 2019WCSD3354WEEK05/Program.cs
@@ -7,32 +7,38 @@ namespace _2019WCSD3354WEEK05
     {
         static void Main(string[] args)
         {
-            var a = new testques2();
-            a.Play();
+            //var a = new testques2();
+            //a.Play();
+            birthday_party b = new birthday_party();
+            b.setupPartyList();
+            b.printPartyList();
+            b.printPartyListreverse();
+            Console.WriteLine(b.printPartyList());
+            Console.WriteLine(b.printPartyListreverse());
         }
     }
 
-    class testques2
-    {
-        public int myfavvari = 0;
-        public void Play()
-        {
-            //write a for loop
+    //class testques2
+    //{
+    //    public int myfavvari = 0;
+    //    public void Play()
+    //    {
+    //        //write a for loop
 
-            for (; my();)
-            {
-                if (myfavvari <= 10)
-                    Console.WriteLine("you are fool");
-                break;
-            }
-            Console.WriteLine("shit for loop again");
-        }
-        public bool my()
-        {
-            myfavvari++;
-            return true;
-        }
-    }
+    //        for (; my();)
+    //        {
+    //            if (myfavvari <= 10)
+    //                Console.WriteLine("you are fool");
+    //            break;
+    //        }
+    //        Console.WriteLine("shit for loop again");
+    //    }
+    //    public bool my()
+    //    {
+    //        myfavvari++;
+    //        return true;
+    //    }
+    //}
 
     class dog
     {
@@ -57,6 +63,8 @@ namespace _2019WCSD3354WEEK05
         public dog peanut;
         public dog fifi;
         public dog clarence;
+        public dog Giselle;
+        public dog Lulu;
         public dog roy;
 
         public dog head;
@@ -68,20 +76,79 @@ namespace _2019WCSD3354WEEK05
             peanut = new dog("Peanut", "Bichon");
             fifi = new dog("Fifi", "Poodle");
             clarence = new dog("Clarence", "German Sheppard");
+            Giselle = new dog("Giselle", "Lebra");
+            Lulu = new dog("Lulu", "Bulldog");
             roy = new dog("Roy", "Beagle");
 
             peanut.prev_dog = null;
             peanut.next_dog = fifi;
+
             fifi.prev_dog = peanut;
             fifi.next_dog = clarence;
+
+
             clarence.prev_dog = fifi;
-            clarence.next_dog = roy;
-            roy.prev_dog = clarence;
+            clarence.next_dog = Giselle;
+
+
+            Giselle.prev_dog = clarence;
+            Giselle.next_dog =Lulu;
+
+
+            Lulu.prev_dog = Giselle;
+            Lulu.next_dog = roy;
+
+            roy.prev_dog = Lulu;
             roy.next_dog = null;
             head = peanut;
             tail = roy;
 
         }
 
+        public string printPartyList()
+
+        {
+
+            string inviteList = "*--";
+
+            temporary = head;
+
+            while (temporary.next_dog != null)
+                
+            {
+                
+                inviteList += temporary.dog_name + " * --- * ";
+                temporary = temporary.next_dog;
+            }
+            inviteList += temporary.dog_name + " * --- * ";
+            return inviteList;
+        }
+
+            public string printPartyListreverse()
+
+            {
+
+                string inviteList = "*--";
+
+                temporary = tail;
+
+                while (temporary.prev_dog != null)
+                
+
+            {
+                   
+                    inviteList += temporary.dog_name + " * --- * ";
+                    temporary = temporary.prev_dog;
+                }
+
+
+
+                return inviteList;
+
+        }
+
+
+
     }
+
 }
